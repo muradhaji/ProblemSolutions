f89fe7a49cc7aba0a3fe819188311aebd207bd76
muradhaji <muradhajiyev42@icloud.com>
2024-06-10 23:49:56 +0400

    LeetCode 2715 refactored

--- LeetCode/js/30 days of javascript/2715.js
+++ LeetCode/js/30 days of javascript/2715.js
@@ -13,28 +13,3 @@ var cancellable = function (fn, args, t) {
     clearInterval(timeoutId);
   };
 };
-
-const result = [];
-
-const fn = (x) => x * 5;
-
-const args = [2],
-  t = 20,
-  cancelTimeMs = 5;
-
-const start = performance.now();
-
-const log = (...argsArr) => {
-  const diff = Math.floor(performance.now() - start);
-  result.push({ time: diff, returned: fn(...argsArr) });
-};
-
-const cancel = cancellable(log, args, t);
-
-const maxT = Math.max(t, cancelTimeMs);
-
-setTimeout(cancel, cancelTimeMs);
-
-setTimeout(() => {
-  console.log(result); // [{"time":20,"returned":10}]
-}, maxT + 15);
